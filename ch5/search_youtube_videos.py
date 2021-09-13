05240bcf9d26261297f901cdf68952fb529099e7
yusuke komatsu <lanos.komatsu@gmail.com>
2021-09-14 08:56:26 +0900

    add

---
+++ ch5/search_youtube_videos.py
@@ -0,0 +1,15 @@
+import os
+from googleapiclient.discovery import build
+
+YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
+
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+
+search_response = youtube.search().list(part='snippet', q='手芸', type='video').execute()
+
+for item in search_response['items']:
+    print(item['snippet']['title'])
+
+
+
+
