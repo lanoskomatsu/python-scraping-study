c0bbc4a23e871079ef84760ccad345a867f442aa
yusuke komatsu <lanos.komatsu@gmail.com>
2021-09-23 23:47:06 +0000

    fix

--- ch5/mechanicalsoup_google.py
+++ ch5/mechanicalsoup_google.py
@@ -1,13 +1,13 @@
 import mechanicalsoup
 
-browser = mechanicalsoup.StatefulBrowser()
+browser = mechanicalsoup.StatefulBrowser(user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0')
 browser.open('')
 
-browser.select_from('form[action="/search"]')
+browser.select_form('form[action="/search"]')
 browser['q'] = 'Python'
 browser.submit_selected()
 
 page = browser.get_current_page()
-for a in page.select('h3 > a'):
+for a in page.select('a > h3'):
     print(a.text)
     print(browser.absolute_url(a.get('href')))
